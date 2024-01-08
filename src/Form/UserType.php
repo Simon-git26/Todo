702f58e33df5052a61c5c00acd7c1860db7398b0
Simon-git26 <simoncestmoi@hotmail.fr>
2024-01-08 12:01:24 +0100

    Choix du role de user quand creation d'un user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -26,6 +27,14 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Roles utilisateur',
+                'choices' => ['ROLE_ADMIN' => 'ROLE_ADMIN', 'ROLE_USER' => 'ROLE_USER'],
+                'required' => true,
+                'multiple' => true,
+                'expanded' => true,
+            ]
+        );
         ;
     }
 

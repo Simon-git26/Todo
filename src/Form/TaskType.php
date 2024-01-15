1fc70749702578c482141161fd5b14e4b2bce373
Simon-git26 <simoncestmoi@hotmail.fr>
2024-01-15 11:28:58 +0100

    Design Creation Tache OK

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -8,15 +8,19 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('createdAt')
-            ->add('title')
-            ->add('content', TextareaType::class)
-            ->add('isDone')
+            ->add('createdAt', DatetimeType::class, ['label' => "Crée le :"])
+            ->add('title', TextType::class, ['label' => "Titre :"])
+            ->add('content', TextareaType::class, ['label' => "Contenu :"])
+            ->add('isDone', CheckboxType::class, ['label' => "Terminé :"])
         ;
     }
 

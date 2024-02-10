c2b71ceec4a8eb3b0b7631afa2f849b7eadc8087
Simon-git26 <simoncestmoi@hotmail.fr>
2024-02-10 10:26:53 +0100

    Ok pour installation de blackfire, mais desactivation de l'extension car pas de profil payant

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -183,7 +183,7 @@ class TaskControllerTest extends WebTestCase
     {
         $this->loginUser();
 
-        $this->client->request('GET', '/tasks/5/toggle');
+        $this->client->request('GET', '/tasks/2/toggle');
 
         // Je m'attend a : Une redirection (vers la page accueil)
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
@@ -207,7 +207,7 @@ class TaskControllerTest extends WebTestCase
     {
         $this->loginUser();
 
-        $this->client->request('GET', '/tasks/5/delete');
+        $this->client->request('GET', '/tasks/2/delete');
 
         // Je m'attend a : Une redirection (vers la page accueil)
         $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
